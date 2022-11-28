db561c7b7ab28e7916c1c7b30ca29cd5bd1f0bea
Andrey Kruglov <kruglov.it.rs@gmail.com>
2022-11-28 03:45:27 +0300

    add initial cards from js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const fioInput = editForm.elements.fio;
 const descriptionInput = editForm.elements.description;
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
+const elements = document.querySelector('.elements');
 
 function openEditProfile() {
   fioInput.value = profileName.textContent;
@@ -29,3 +30,43 @@ function handleEditFormSubmit(evt) {
 }
 
 editForm.addEventListener('submit', handleEditFormSubmit);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+function createCard(name, link) {
+  const elementTemplate = document.querySelector('#element-template').content;
+  const element = elementTemplate.querySelector('.element').cloneNode(true);
+  const elementImage = element.querySelector('.element__image');
+  const elementTitle = element.querySelector('.element__title');
+  elementImage.src = link;
+  elementTitle.textContent = name;
+
+  return element;
+}
+
+initialCards.forEach(({name, link}) => elements.append(createCard(name, link)));
