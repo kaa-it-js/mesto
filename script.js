9a7e17dc28ed58f474ad1188ae1f9ab3d582d042
Andrey Kruglov <kruglov.it.rs@gmail.com>
2022-11-28 03:15:55 +0300

    fill edit profile form

---
+++ script.js
@@ -0,0 +1,22 @@
+const profileEditButton = document.querySelector('.profile__edit-button');
+const popupCloseButton = document.querySelector('.popup__close-button');
+const popup = document.querySelector('.popup');
+const editForm = document.forms.editForm;
+const fioInput = editForm.elements.fio;
+const descriptionInput = editForm.elements.description;
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
+
+function openEditProfile() {
+  fioInput.value = profileName.textContent;
+  descriptionInput.value = profileDescription.textContent;
+
+  popup.classList.add('popup_opened');
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+profileEditButton.addEventListener('click', openEditProfile);
+popupCloseButton.addEventListener('click', closePopup);
