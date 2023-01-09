62af034b9cdfe9d280417a8467a55b8ca433eedf
Andrey Kruglov <kruglov.it@gmail.com>
2023-01-09 15:54:56 +0300

    with likes

--- src/components/card.js
+++ src/components/card.js
@@ -1,53 +1,35 @@
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
+import api from "./api";
 
 const elements = document.querySelector(".elements");
 const imagePopupImage = document.querySelector(".image-popup__image");
 const imagePopupName = document.querySelector(".image-popup__name");
 const imagePopup = document.querySelector(".image-popup").closest(".popup");
 
-export const initCards = () => {
-  initialCards.forEach(({ name, link }) =>
-    elements.append(createCard(name, link))
-  );
+export const initCards = (cards, profile) => {
+  console.dir(new Set(cards.flatMap((c) => c.likes.map((like) => like._id))));
+  console.log(profile._id);
+  cards.forEach((card) => elements.append(createCard(card, profile)));
 };
 
-export const createCard = (name, link) => {
+export const createCard = (card, { _id }) => {
   const elementTemplate = document.querySelector("#element-template").content;
   const element = elementTemplate.querySelector(".element").cloneNode(true);
   const elementImage = element.querySelector(".element__image");
   const elementTitle = element.querySelector(".element__title");
-  elementImage.src = link;
-  elementTitle.textContent = name;
+  elementImage.src = card.link;
+  elementTitle.textContent = card.name;
 
   const likeButton = element.querySelector(".element__like-button");
+  const likeCounter = element.querySelector(".element__like-counter");
+
+  likeCounter.textContent = String(card.likes.length);
+
+  if (card.likes.some((like) => like._id === _id)) {
+    likeButton.classList.add("element__like-button_active");
+  }
 
   likeButton.addEventListener("click", () =>
-    likeButton.classList.toggle("element__like-button_active")
+    handleLike(likeButton, likeCounter, card._id)
   );
 
   element.addEventListener("click", function (evt) {
@@ -56,11 +38,33 @@ export const createCard = (name, link) => {
     }
 
     if (evt.target.classList.contains("element__image")) {
-      openImagePopup(name, link);
+      openImagePopup(card.name, card.link);
     }
   });
 
   return element;
+};
+
+const handleLike = (likeButton, likeCounter, cardId) => {
+  if (likeButton.classList.contains("element__like-button_active")) {
+    // Снимаем лайк
+    api
+      .deleteLike(cardId)
+      .then((card) => {
+        likeCounter.textContent = String(card.likes.length);
+      })
+      .catch((err) => console.log(err));
+  } else {
+    // Ставим лайк
+    api
+      .setLike(cardId)
+      .then((card) => {
+        likeCounter.textContent = String(card.likes.length);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  likeButton.classList.toggle("element__like-button_active");
 };
 
 const openImagePopup = (name, link) => {
